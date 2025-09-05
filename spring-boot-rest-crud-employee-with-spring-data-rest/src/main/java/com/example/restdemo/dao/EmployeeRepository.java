0d849c90b67e8819febe3fa9f7b4000d0620ac7a
ifshaan02 <ifshaan02@gmail.com>
2025-09-05 23:46:01 +0530

    Add files via upload

---
+++ spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/com/example/restdemo/dao/EmployeeRepository.java
@@ -0,0 +1,14 @@
+package com.example.restdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.example.restdemo.entity.Employee;
+
+
+// @RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+	
+	// no need to write any code
+
+}
